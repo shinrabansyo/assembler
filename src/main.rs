14457a044201e7ca95a3ab1874a677addc1e5f49
wakuto <waku.902002@gmail.com>
2023-12-28 21:32:55 +0900

    分岐命令のテストを行うプログラムに変更
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -138,21 +138,27 @@ macro_rules! assembly {
 #[rustfmt::skip]
 fn main() {
     vec![
-        assembly!(lw r6 = r0[0]),
-        assembly!(lw r7 = r0[4]),
+        assembly!(addi r1 = r0, 1),                // 00 (00)
+        assembly!(bne r0, (r0, r1) -> 0),          // 06 (06)
+        assembly!(addi r1 = r1, 1),                // 12 (0C)
 
-        assembly!(sw r0[0] = r7),
-        assembly!(sw r0[4] = r6),
 
-        assembly!(addi r5 = r5, 1),
-        assembly!(addi r0 = r0, 0),
-        assembly!(addi r1 = r1, 1),
+        // assembly!(lw r6 = r0[0]),               // 00 (00)
+        // assembly!(lw r7 = r0[4]),               // 06 (06)
 
-        assembly!(add  r2 = r0, r1),
-        assembly!(addi r0 = r1, 0),
-        assembly!(addi r1 = r2, 0),
-        assembly!(addi r5 = r5, 1),
-        assembly!(beq  r4, (r3, r3) -> -24),
+        // assembly!(sw r0[0] = r7),               // 12 (0C)
+        // assembly!(sw r0[4] = r6),               // 18 (12)
+
+        // assembly!(addi r5 = r5, 1),             // 24 (18)
+        // assembly!(addi r0 = r0, 0),             // 30 (1E)
+        // assembly!(addi r1 = r1, 1),             // 36 (24)
+
+        // assembly!(add  r2 = r0, r1),            // 42 (2A)
+        // assembly!(addi r0 = r1, 0),             // 48 (30)
+        // assembly!(addi r1 = r2, 0),             // 54 (36)
+        // assembly!(addi r5 = r5, 1),             // 60 (3C)
+        // assembly!(beq  r4, (r3, r3) -> -24),    // 66 (42)
+        //                                         // 72 (48)
     ]
     .into_iter()
     .for_each(|inst| println!("{}", inst));
