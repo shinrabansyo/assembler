afcfa88db1a76858641d3d76f415edd30f260d92
wakuto <waku.902002@gmail.com>
2024-06-02 22:55:01 +0900

    ソースファイルからの入力に対応
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>

--- crates/assembler/src/lib.rs
+++ crates/assembler/src/lib.rs
@@ -13,22 +13,22 @@ pub fn assemble(insts: Vec<Inst>) -> String {
 macro_rules! assembly {
     // add, addi, ...
     ($kind:ident $rd:ident = $rs1:expr, $rs2:expr) => {
-        Inst::from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($rs2)]))
+        Inst::try_from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($rs2)])).unwrap()
     };
 
     // beq, ...
     ($kind:ident $rd:ident, ($rs1:ident, $rs2:ident) -> $imm:expr) => {
-        Inst::from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($rs2), stringify!($imm)]))
+        Inst::try_from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($rs2), stringify!($imm)])).unwrap()
     };
 
     // lw, ..., in
     ($kind:ident $rd:ident = $rs1:ident[$imm:expr]) => {
-        Inst::from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($imm)]))
+        Inst::try_from((stringify!($kind), vec![stringify!($rd), stringify!($rs1), stringify!($imm)])).unwrap()
     };
 
     // sw, ..., out
     ($kind:ident $rs1:ident[$imm:expr] = $rs2:ident) => {
-        Inst::from((stringify!($kind), vec![stringify!($rs1), stringify!($imm), stringify!($rs2)]))
+        Inst::try_from((stringify!($kind), vec![stringify!($rs1), stringify!($imm), stringify!($rs2)])).unwrap()
     };
 }
 
@@ -59,9 +59,11 @@ pub enum Inst {
     Out { rs1: u8, rs2: u8, imm: i32 },
 }
 
-impl From<(&str, Vec<&str>)> for Inst {
+impl TryFrom<(&str, Vec<&str>)> for Inst {
+    type Error = anyhow::Error;
+
     #[rustfmt::skip]
-    fn from((kind, args): (&str, Vec<&str>)) -> Self {
+    fn try_from((kind, args): (&str, Vec<&str>)) -> anyhow::Result<Self> {
         let args = args
             .into_iter()
             .map(|arg| arg.replace("r", ""))
@@ -69,39 +71,114 @@ impl From<(&str, Vec<&str>)> for Inst {
             .collect::<Vec<_>>();
 
         match kind {
-            "add" => Inst::Add { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8 },
-            "sub" => Inst::Sub { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8 },
+            "add" => Ok(Inst::Add { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8 }),
+            "sub" => Ok(Inst::Sub { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8 }),
 
-            "addi" => Inst::Addi { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] as u32 },
-            "subi" => Inst::Subi { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] as u32 },
+            "addi" => Ok(Inst::Addi { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] as u32 }),
+            "subi" => Ok(Inst::Subi { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] as u32 }),
 
-            "beq" => Inst::Beq { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] },
-            "bne" => Inst::Bne { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] },
-            "blt" => Inst::Blt { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] },
-            "ble" => Inst::Ble { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] },
+            "beq" => Ok(Inst::Beq { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] }),
+            "bne" => Ok(Inst::Bne { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] }),
+            "blt" => Ok(Inst::Blt { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] }),
+            "ble" => Ok(Inst::Ble { rd: args[0] as u8, rs1: args[1] as u8, rs2: args[2] as u8, imm: args[3] }),
 
-            "lw" => Inst::Lw { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
-            "lh" => Inst::Lh { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
-            "lb" => Inst::Lb { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
-            "lhu" => Inst::Lhu { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
-            "lbu" => Inst::Lbu { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
+            "lw" => Ok(Inst::Lw { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
+            "lh" => Ok(Inst::Lh { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
+            "lb" => Ok(Inst::Lb { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
+            "lhu" => Ok(Inst::Lhu { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
+            "lbu" => Ok(Inst::Lbu { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
 
-            "sw" => Inst::Sw { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 },
-            "sh" => Inst::Sh { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 },
-            "sb" => Inst::Sb { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 },
+            "sw" => Ok(Inst::Sw { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 }),
+            "sh" => Ok(Inst::Sh { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 }),
+            "sb" => Ok(Inst::Sb { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 }),
 
-            "in" => Inst::In { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] },
-            "out" => Inst::Out { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 },
+            "in" => Ok(Inst::In { rd: args[0] as u8, rs1: args[1] as u8, imm: args[2] }),
+            "out" => Ok(Inst::Out { rs1: args[0] as u8, imm: args[1], rs2: args[2] as u8 }),
 
-           _ => unimplemented!(),
+           _ => Err(anyhow::anyhow!("Invalid instruction: {}", kind)),
         }
     }
 }
 
+impl TryFrom<&str> for Inst {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &str) -> anyhow::Result<Inst> {
+        let (lhs, rhs) = if s.contains("->") {
+            // beq, ...
+            let splitted_by_arrow = s.split("->").collect::<Vec<_>>();
+            (splitted_by_arrow[0], splitted_by_arrow[1])
+        } else {
+            // add, lw, sw, in, out, ...
+            let splitted_by_eq = s.split("=").collect::<Vec<_>>();
+            (splitted_by_eq[0], splitted_by_eq[1])
+        };
+        let lhs = lhs.split_ascii_whitespace().collect::<Vec<_>>();
+        let (kind, lhs) = (lhs[0], lhs[1..].concat());
+
+        // beq
+        if lhs.contains("(") {
+            let rhs = rhs.trim();
+            let lhs = lhs
+                .split(",")
+                .map(|e| e.trim())
+                .collect::<Vec<_>>();
+            // beq r0, (r0, r0) -> -42
+            // beq | r0, (r0, r0) | -42
+            // beq | r0, (r0, r0) | -42
+            // beq | r0 | (r0 | r0) | -42
+            return Inst::try_from((kind, vec![lhs[0], &lhs[1].replace("(", ""), &lhs[2].replace(")", ""), rhs]));
+        }
+
+        // sw, ..., out
+        if lhs.contains("[") {
+            let rhs = rhs.trim();
+            let lhs = lhs
+                .split("[")
+                .map(|e| e.trim())
+                .collect::<Vec<_>>();
+            // sw r0[4] = r7
+            // sw r0[4] | r7
+            // sw r0 | 4] | r7
+            return Inst::try_from((kind, vec![lhs[0], &lhs[1].replace("]", ""), rhs]));
+        }
+
+        // lw, ..., in
+        if rhs.contains("[") {
+            let lhs = lhs.trim();
+            let rhs = rhs
+                .split("[")
+                .map(|e| e.trim())
+                .collect::<Vec<_>>();
+            // lw r7 = r0[4]
+            // lw | r7 | r0[4]
+            // lw | r7 | r0 | 4]
+            return Inst::try_from((kind, vec![lhs, rhs[0], &rhs[1].replace("]", "")]));
+        }
+
+        // アセンブリ言語例
+        // assembly!(out r0[4] = r1),
+        // assembly!(addi r1 = r0, 1),                // 00 (00)
+        // assembly!(beq r0, (r0, r0) -> -42),
+        // assembly!(lw r7 = r0[4]),               // 06 (06)
+        // assembly!(sw r0[0] = r7),               // 12 (0C)
+
+        // add, addi, ...
+        let lhs = lhs.trim();
+        let rhs = rhs
+            .split(",")
+            .map(|e| e.trim())
+            .collect::<Vec<_>>();
+
+        Inst::try_from((kind, vec![lhs, rhs[0], rhs[1]]))
+    }
+}
+
 impl TryFrom<u64> for Inst {
-    type Error = ();
+    type Error = anyhow::Error;
+
     #[rustfmt::skip]
-    fn try_from(inst: u64) -> Result<Inst, Self::Error> {
+    fn try_from(inst: u64) -> anyhow::Result<Inst> {
         let opcode     = ((inst >> 0) & 0x1F) as u8;
         let opcode_sub = ((inst >> 5) & 0x07) as u8;
         let rd         = ((inst >> (5+3)) & 0x1F) as u8;
@@ -115,21 +192,21 @@ impl TryFrom<u64> for Inst {
             0x00 => {
                 match opcode_sub {
                     0x00 => Ok(Inst::Add { rd: 0, rs1: 0, rs2: 0 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x01 => {
                 match opcode_sub {
                     0x01 => Ok(Inst::Add { rd, rs1: rs1_r, rs2 }),
                     0x02 => Ok(Inst::Sub { rd, rs1: rs1_r, rs2 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x02 => {
                 match opcode_sub {
                     0x01 => Ok(Inst::Addi { rd, rs1: rs1_i_s, imm }),
                     0x02 => Ok(Inst::Subi { rd, rs1: rs1_i_s, imm }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x03 => {
@@ -138,7 +215,7 @@ impl TryFrom<u64> for Inst {
                     0x01 => Ok(Inst::Bne { rd, rs1: rs1_r, rs2, imm: imm as i32 }),
                     0x02 => Ok(Inst::Blt { rd, rs1: rs1_r, rs2, imm: imm as i32 }),
                     0x03 => Ok(Inst::Ble { rd, rs1: rs1_r, rs2, imm: imm as i32 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x04 => {
@@ -148,7 +225,7 @@ impl TryFrom<u64> for Inst {
                     0x02 => Ok(Inst::Lb  { rd, rs1: rs1_i_s, imm: imm as i32 }),
                     0x03 => Ok(Inst::Lhu { rd, rs1: rs1_i_s, imm: imm as i32 }),
                     0x04 => Ok(Inst::Lbu { rd, rs1: rs1_i_s, imm: imm as i32 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x05 => {
@@ -156,17 +233,17 @@ impl TryFrom<u64> for Inst {
                     0x00 => Ok(Inst::Sw { rs1: rs1_i_s, rs2, imm: imm as i32 }),
                     0x01 => Ok(Inst::Sh { rs1: rs1_i_s, rs2, imm: imm as i32 }),
                     0x02 => Ok(Inst::Sb { rs1: rs1_i_s, rs2, imm: imm as i32 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
             0x06 => {
                 match opcode_sub {
                     0x00 => Ok(Inst::In  { rd, rs1: rs1_i_s, imm: imm as i32 }),
                     0x01 => Ok(Inst::Out { rs1: rs1_i_s, rs2, imm: imm as i32 }),
-                    _ => Err(()),
+                    _ => Err(anyhow::anyhow!("Invalid opcode_sub")),
                 }
             },
-            _ => Err(()),
+            _ => Err(anyhow::anyhow!("Invalid opcode")),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,31 @@
 use assembler::{assemble, assembly, Inst};
 use compiler::compile;
+use std::env;
+use std::fs;
 
 #[rustfmt::skip]
 fn main() {
-    // let program = "
-    //     hello, shirabansyo!
-    // ";
-    // let result = assemble(compile(program));
-    // println!("{}", result);
+    let args = env::args().collect::<Vec<String>>();
 
-    let result = assemble(vec![
-        /*
-            | 0x00000001    | SPI (Data)
-            | 0x00000002    | SPI (Mode)
-            | 0x00000003    | SPI (Clockshamt)
-        */
+    if args.len() < 2 {
+        println!("Usage: {} [path/to/source]", args[0]);
+        return;
+    }
+    
+    let source = fs::read_to_string(&args[1]).unwrap();
+    
+    let lines = source
+        .split("\n")
+        .map(|line| line.trim())
+        .map(|line| line.split("//").collect::<Vec<_>>()[0])
+        .filter(|line| !line.is_empty())
+        .collect::<Vec<&str>>();
 
-        /*
-           # mode
-            out r0[2] = r0
-           # clkshamt 
-            addi r1 = r0, 4
-            out r0[3] = r1
-           
-           # A: 97, Z: 122
-            addi r2 = r0, 97
-            addi r3 = r0, 123
-            out r0[1] = r2
-            addi r2 = r2, 1
-            beq r0, (r2, r3) -> -24
-            beq r0, (r0, r0) -> -18
-
-        */
-
-        /*
-        int main(void) {
-            *GPIO = 0;
-            *SPI_MODE = 0;
-            *SPI_CSHMAT = 0;
-
-            char c = 'a';
-            while (1) {
-                *GPIO = 1;
-                *SPI_DATA = c;
-                *GPIO = 0;
-
-                if (c == 'z') {
-                    c = 'a';
-                } else {
-                    ++ c;
-                }
-            }
-        }
-        */
-
-        // Gpio:Init
-        assembly!(addi r1 = r0, 0),
-        assembly!(out r0[4] = r1),
-
-        // Spi:Mode
-        assembly!(addi r1 = r0, 3),
-        assembly!(out r0[2] = r1),
-
-        // Spi:Clockshamt
-        assembly!(addi r1 = r0, 4),
-        assembly!(out r0[3] = r1),
-
-        // Loop:Setup
-        assembly!(addi r2 = r0, 97),
-        assembly!(addi r3 = r0, 123),
-
-        // Spi:CS
-        assembly!(addi r1 = r0, 1),
-        assembly!(out r0[4] = r1),
-
-        // Spi:Send
-        assembly!(out r0[1] = r2),
-
-        // Spi:CS
-        assembly!(addi r1 = r0, 0),
-        assembly!(out r0[4] = r1),
-
-        // Loop
-        assembly!(addi r2 = r2, 1),
-        assembly!(beq r0, (r2, r3) -> -48),
-        assembly!(beq r0, (r0, r0) -> -42),   
-
-        // assembly!(addi r1 = r0, 1),                // 00 (00)
-        // assembly!(bne r0, (r0, r1) -> 0),          // 06 (06)
-        // assembly!(addi r1 = r1, 1),                // 12 (0C)
-
-        // assembly!(addi r2 = r0, 97),                // 00 (00)
-        // assembly!(out r0[0] = r2),                  // 06 (06)
-
-        // `out(x[rs1] + imm, rs2)`
-        // assembly!(out rs1[imm] = rs2),
-        // `rd = in(x[rs1] + imm)`
-        // assembly!(in rd = rs1[imm]),
-
-        // assembly!(lw r6 = r0[0]),               // 00 (00)
-        // assembly!(lw r7 = r0[4]),               // 06 (06)
-
-        // assembly!(sw r0[0] = r7),               // 12 (0C)
-        // assembly!(sw r0[4] = r6),               // 18 (12)
-
-        // assembly!(addi r5 = r5, 1),             // 24 (18)
-        // assembly!(addi r0 = r0, 0),             // 30 (1E)
-        // assembly!(addi r1 = r1, 1),             // 36 (24)
-
-        // assembly!(add  r2 = r0, r1),            // 42 (2A)
-        // assembly!(addi r0 = r1, 0),             // 48 (30)
-        // assembly!(addi r1 = r2, 0),             // 54 (36)
-        // assembly!(addi r5 = r5, 1),             // 60 (3C)
-        // assembly!(beq  r4, (r3, r3) -> -24),    // 66 (42)
-        //                                         // 72 (48)
-    ]);
-    println!("{}", result);
+    let programs = lines
+        .into_iter()
+        .map(|line| Inst::try_from(line))
+        .collect::<anyhow::Result<Vec<Inst>>>()
+        .unwrap();
+    
+    println!("{}", assemble(programs));
 }
