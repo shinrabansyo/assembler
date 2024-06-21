3bc739eeba2de9a26e5ef02c138971bca5ae9de9
wakuto <waku.902002@gmail.com>
2024-06-21 23:59:48 +0900

    ラベルの存在確認と位置算出
    Co-authored-by: Sato Koichiro <kouichi310@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>

--- crates/assembler/src/check.rs
+++ crates/assembler/src/check.rs
@@ -1,5 +1,23 @@
-use crate::ir::unresolved::Inst;
+use crate::ir::unresolved::{Inst, InstKind};
 
 pub fn check(insts: &[Inst]) -> anyhow::Result<()> {
+    let labels = insts.iter()
+        .filter(|inst| inst.label.is_some())
+        .map(|inst| inst.label.clone().unwrap())
+        .collect::<Vec<String>>();
+
+    for inst in insts {
+        let label = match &inst.kind {
+            InstKind::Beq { label, .. } => label,
+            InstKind::Bne { label, .. } => label,
+            InstKind::Blt { label, .. } => label,
+            InstKind::Ble { label, .. } => label,
+            _ => continue,
+        };
+        
+        if !labels.contains(label) {
+            return Err(anyhow::anyhow!("label {} is not found", label));
+        }
+    }
     Ok(())
 }
--- crates/assembler/src/resolve.rs
+++ crates/assembler/src/resolve.rs
@@ -1,5 +1,61 @@
 use crate::ir::{unresolved, resolved};
+use crate::ir::unresolved::InstKind;
+use std::collections::HashMap;
 
 pub fn resolve(insts: Vec<unresolved::Inst>) -> anyhow::Result<Vec<resolved::Inst>> {
+    let mut label_map = HashMap::new();
+    for (idx, inst) in insts.iter().enumerate() {
+        // let label = match &inst.kind {
+        //     InstKind::Beq { label, .. } => label,
+        //     InstKind::Bne { label, .. } => label,
+        //     InstKind::Blt { label, .. } => label,
+        //     InstKind::Ble { label, .. } => label,
+        //     _ => continue,
+        // };
+        if inst.label.is_some() {
+            label_map.insert(inst.label.clone().unwrap(), idx*6);
+        }
+    }
+
+
+
     unimplemented!()
 }
+
+
+/*
+let mut map = ...;
+map.insert(K, V);
+
+.is_some()
+.is_none()
+*/
+
+
+/*
+// Loop:Setup
+label    kind
+  ↓       ↓
+@setup   addi r2 = r0, 97                0
+[noname] addi r3 = r0, 123               6
+
+// Spi:CS
+@loop    addi r1 = r0, 1                 12
+[noname] out r0[4] = r1                  18
+
+// Spi:Send
+[noname] out r0[1] = r2                  24
+
+// Spi:CS
+[noname] addi r1 = r0, 0                 30
+[noname] out r0[4] = r1                  36
+
+// Loop
+[noname] addi r2 = r2, 1                 42
+[noname] beq r0, (r2, r3) -> setup       48
+[noname] beq r0, (r0, r0) -> loop        54
+
+// ↓みたいに書き換えたい
+[noname] beq r0, (r2, r3) -> 0-48       48
+[noname] beq r0, (r0, r0) -> 12-54      54
+*/
