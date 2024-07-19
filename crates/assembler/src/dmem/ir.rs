05827581f6d10550e8de6bc4310af49714171a82
wakuto <waku.902002@gmail.com>
2024-07-20 00:00:21 +0900

    セクションの切り分けに対応
    Co-authored-by: Sato Koichiro <kouichi310@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Kyuta <Kyuta211@users.noreply.github.com>

---
+++ crates/assembler/src/dmem/ir.rs
@@ -0,0 +1,2 @@
+pub mod unresolved;
+pub mod resolved;
--- crates/assembler/src/lib.rs
+++ crates/assembler/src/lib.rs
@@ -1,6 +1,29 @@
 mod imem;
 mod dmem;
 
-pub fn assemble(program: &str) -> anyhow::Result<String> {
-    imem::assemble(program) 
+mod check;
+mod resolve;
+mod convert;
+
+use check::check;
+use resolve::resolve;
+use convert::convert;
+
+pub fn assemble(program: &str) -> anyhow::Result<String> { 
+    let lines = program.lines();
+    let sep_pos = lines.clone().position(|line| line == "===").unwrap();
+
+    let lines = lines.collect::<Vec<_>>();
+    let dmem_section = &lines[..sep_pos].join("\n");
+    let imem_section = &lines[sep_pos+1..].join("\n");
+
+    // dmem のパース
+
+    // imem のパース
+    let insts = imem::parse(&imem_section)?;
+    
+    // 合流
+    check(&insts)?;
+    let insts = resolve(insts)?;
+    convert(insts)
 }
